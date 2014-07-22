a41258152b8fa0c746e2346ee34f22eb9b23d705
Christopher Grebs <cg@webshox.org>
2014-07-22 06:39:28 +0200

    fix lazyloading

--- src/pytest_django_casperjs/fixtures.py
+++ src/pytest_django_casperjs/fixtures.py
@@ -3,14 +3,15 @@ import os
 import pytest
 from pytest_django.lazy_django import skip_if_no_django
 
-from pytest_django_casperjs.helper import CasperJSLiveServer
-
 
 @pytest.fixture(scope='session')
 def casper_js(request):
     skip_if_no_django()
 
+    from pytest_django_casperjs.helper import CasperJSLiveServer
+
     addr = request.config.getvalue('liveserver')
+
     if not addr:
         addr = os.getenv('DJANGO_TEST_LIVE_SERVER_ADDRESS')
     if not addr:
