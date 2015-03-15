95a5a348f0ede1619a9da69641da46d68bd510b9
Christopher Grebs <cg@webshox.org>
2015-03-15 04:15:49 +0100

    Add proper exclude for 2.6 and django 1.7

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ import os.path
 
 def pytest_configure(config):
     test_db = os.environ.get('DB', 'sqlite')
-    if test_db == 'postgres':
+    if test_db == 'postgresql':
         os.environ['DJANGO_SETTINGS_MODULE'] = 'pytest_django_casperjs.tests.settings_postgresql'  # noqa
         settings.DATABASES['default'].update({
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -26,4 +26,4 @@ def pytest_configure(config):
             'NAME': ':memory:',
         })
     else:
-        raise RuntimeError('Unsupported database configuration')
+        raise RuntimeError('Unsupported database configuration %s' % test_db)
